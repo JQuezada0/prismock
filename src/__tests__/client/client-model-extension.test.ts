373b19ae05ee4bf1fc9c5b83e7b348370f09c1d1
Johnil Quezada <johnilquezada@gmail.com>
2025-11-26 07:33:08 -0500

    update test, export prismock client type

--- src/__tests__/client/client-model-extension.test.ts
+++ src/__tests__/client/client-model-extension.test.ts
@@ -1,7 +1,8 @@
-import { Prisma } from "@prisma/client"
-import { createPrismock } from "../../lib/client"
+import { Prisma, PrismaClient, Role } from "@prisma/client"
+import { createPrismock, type PrismockClientType } from "../../lib/client"
 import { mockDeep } from "vitest-mock-extended"
 import { describe, it, expect, beforeAll } from "vitest"
+import { resetDb } from "../../../testing"
 
 describe("client-model-extension", () => {
   describe("model extension", async () => {
@@ -38,18 +39,20 @@ describe("client-model-extension", () => {
     it("should work with extension definition as function", async () => {
       const prismock = await createPrismock()
 
+      const user = mockDeep<Prisma.$UserPayload['scalars']>({
+        // @ts-expect-error - id is a string in mongodb and a number in postgres
+        id: process.env.TEST_DB_TYPE === "mongodb" ? "1" : 1,
+        email: "extendedClient@foobar.com",
+      })
+
       const extensions = Prisma.defineExtension((client) => {
         return client.$extends({
           model: {
             user: {
-              findManyExtended: () => {
-                const user = mockDeep<Prisma.$UserPayload['scalars']>({
-                  // @ts-expect-error - id is a string in mongodb and a number in postgres
-                  id: process.env.TEST_DB_TYPE === "mongodb" ? "1" : 1,
-                  email: "extendedClient@foobar.com",
-                })
-        
-                return [user]
+              findManyExtended() {
+                const userModel = Prisma.getExtensionContext(this)
+
+                return userModel.findMany()
               },
             },
           }
@@ -58,7 +61,24 @@ describe("client-model-extension", () => {
 
       const extendedPrismock = prismock.$extends(extensions)
 
-      const users = extendedPrismock.user.findManyExtended()
+      await prismock.user.create({
+        data: {
+          email: "extendedClient@foobar.com",
+          // @ts-expect-error - id is a string in mongodb and a number in postgres
+          id: process.env.TEST_DB_TYPE === "mongodb" ? "1" : 1,
+          password: "password",
+          role: Role.USER,
+          warnings: 0,
+          banned: false,
+          money: BigInt(0),
+          friends: 0,
+          signal: null,
+          parameters: {},
+          birthday: null,
+        },
+      })
+
+      const users = await extendedPrismock.user.findManyExtended()
 
       expect(users).toHaveLength(1)
       expect(users[0]).toMatchObject({
