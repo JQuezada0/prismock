df97158d57bed1286038c71fbff9955d0550f778
Johnil Quezada <johnilquezada@gmail.com>
2025-12-04 22:28:00 -0500

    add logging

--- src/__tests__/client/example-prisma.test.ts
+++ src/__tests__/client/example-prisma.test.ts
@@ -1,16 +1,18 @@
-import { it, expect, beforeAll } from "vitest"
+import { it } from "vitest"
 import { describe } from "../../../testing/helpers"
 
 import { simulateSeed } from '../../../testing';
 
-describe('Example', ({ prisma, reset }) => {
+describe('Example', ({ prisma, reset, beforeAll }) => {
   describe('Without mock', () => {
     beforeAll(async () => {
       await reset()
       await simulateSeed(prisma);
     });
 
-    it('Should throw as user email is taken', async () => {
+    it('Should throw as user email is taken', async ({ expect }) => {
+      const users = await prisma.user.findMany()
+      console.log("EXISTING USERS!", users)
       await expect(async () => prisma.user.create({ data: { email: 'user1@company.com', password: 'password' } })).rejects.toThrow();
     });
   });
